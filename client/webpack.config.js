30119faf6f4a30895a94b01ed66e2df5ba220195
Alexander Pourmirzayouf <fastcgicode@yahoo.com>
2024-02-16 19:52:07 -0800

    upd

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,13 +19,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+       title: 'TextEditor PWA',
+       template: "index.html",
+      }),
+     new WorkboxPlugin.GenerateSW({
+       // these options encourage the ServiceWorkers to get in there fast
+       // and not allow any straggling "old" SWs to hang around
+       clientsClaim: true,
+       skipWaiting: true,
+     }),
     ],
-
     module: {
       rules: [
-        
-      ],
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /(node_modules|bower_components|build)/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|less)$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
     },
   };
 };
